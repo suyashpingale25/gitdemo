7fbab47e6cd4efac8c681d23c0a29712d3fad24e
suyashpingale25 <126105251+suyashpingale25@users.noreply.github.com>
2023-07-03 21:53:18 +0530

    Add files via upload

---
+++ Spring_Paypal_Payment_Gateway/Spring_Paypal_Payment_Gateway/src/main/java/com/example/Spring_Paypal_Payment_Gateway/SpringPaypalPaymentGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Paypal_Payment_Gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPaypalPaymentGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPaypalPaymentGatewayApplication.class, args);
+	}
+
+}
---
+++ Spring_Paypal_Payment_Gateway/Spring_Paypal_Payment_Gateway/src/test/java/com/example/Spring_Paypal_Payment_Gateway/SpringPaypalPaymentGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Paypal_Payment_Gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPaypalPaymentGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
